d0db2af7f50bb4439b19ec3b6564065295eb6047
Muhammad Naveed <149371965+naveed-devOps@users.noreply.github.com>
2024-02-21 15:46:07 +0500

    update userModel

--- backend/api/models/userModel.js
+++ backend/api/models/userModel.js
@@ -33,5 +33,5 @@ const userSchema = new mongoose.Schema({
         },
     ],
 })
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+
+export default mongoose.model('User', userSchema);
