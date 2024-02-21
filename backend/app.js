c46ef0b835e35fea3bddfb26db4c2aec1331fa6c
Muhammad Naveed <149371965+naveed-devOps@users.noreply.github.com>
2024-02-21 14:46:46 +0500

    add folder structure

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose'; 
+import cors from 'cors';
+
+
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 65;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// // api
+// app.use('/api', userRoute);
+
+// MongoDB connection
+mongoose.connect(MONGO_URL).then(() => {
+        console.log('Connected to MongoDB');
+
+        // Server is running after MongoDB connection
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+          });
+          
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+ 
