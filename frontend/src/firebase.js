19a91b02319697224b2d8d063b24ba29c7d6b8a4
Muhammad Naveed <149371965+naveed-devOps@users.noreply.github.com>
2024-02-27 22:58:51 +0500

    add firebase configration

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDP_k87hDSX-VaCeCynVC8Jeii-kO_k1PI",
-    authDomain: "linkedin-clone-91b13.firebaseapp.com",
-    projectId: "linkedin-clone-91b13",
-    storageBucket: "linkedin-clone-91b13.appspot.com",
-    messagingSenderId: "1097521299525",
-    appId: "1:1097521299525:web:ed511f4425800ed386632c"
+    apiKey: "AIzaSyB3jzUvJfY5HADXW1Uh6k7CscHp823twWI",
+  authDomain: "discord-clone-15794.firebaseapp.com",
+  projectId: "discord-clone-15794",
+  storageBucket: "discord-clone-15794.appspot.com",
+  messagingSenderId: "374084316470",
+  appId: "1:374084316470:web:fd071e0335a65a166ed797"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
